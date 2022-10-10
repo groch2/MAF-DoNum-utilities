6ed088f50a624a57af48a1dd629822ada505af86
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-10-10 15:59:44 +0200

    défnir les tryptiques des documents de test

---
+++ update_all_documents_from_json/update_tryptiques.mjs
@@ -0,0 +1,32 @@
+import * as fs from 'fs/promises'
+import json_object from 'file://C:/Users/deschaseauxr/Documents/DONUM/documents.json' assert { type: 'json' };
+import { areStringsEqualsCaseInsensitive } from '../string_compare.mjs'
+const tryptiques = [
+  {
+    famille: 'DOCUMENTS COCOON',
+    cote: 'ENCAISSEMENT',
+    type: 'BORDEREAU DE REMISE EN BANQUE'
+  },
+  {
+    famille: 'DOCUMENTS CONTRAT',
+    cote: 'PROCEDURE COLLECTIVE',
+    type: 'A.R. REVENU SIGNE'
+  },
+  {
+    famille: 'DOCUMENTS EMOA',
+    cote: 'ATTESTATIONS ASSURANCES',
+    type: 'ATTESTATION ASSURANCE'
+  }
+]
+json_object.value = json_object.value.map(doc => {
+  const { cote, type } = tryptiques.find(({ famille }) => areStringsEqualsCaseInsensitive(famille, doc.categoriesFamille))
+  return {
+    ...doc,
+    categoriesCote: cote,
+    categoriesTypeDocument: type,
+  }
+})
+const stringified = JSON.stringify(json_object, undefined, 2)
+const file_path = 'C:/Users/deschaseauxr/Documents/DONUM/documents.json'
+await fs.writeFile(file_path, stringified, { encoding: 'utf8' })
+console.log('terminé')
