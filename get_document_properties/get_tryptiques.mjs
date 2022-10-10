e124d4c41b325f432f4a7119be637f23f83d192c
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-10-10 17:30:36 +0200

    récupérer les tryptiques valides, triées

--- get_document_properties/get_tryptiques.mjs
+++ get_document_properties/get_tryptiques.mjs
@@ -1,7 +1,11 @@
 import familles from './tryptiques/familles.json' assert { type: 'json' }
 import cotes from './tryptiques/cotes.json' assert { type: 'json' }
 import types from './tryptiques/types.json' assert { type: 'json' }
-import { areStringsEqualsCaseInsensitive } from '../string_compare.mjs'
+import * as fs from 'fs/promises'
+
+export function localCompareCaseInsensitive(a, b) {
+  return a?.localeCompare(b, undefined, { sensitivity: 'accent' });
+}
 
 const tryptiques =
   types.value
@@ -13,22 +17,16 @@ const tryptiques =
             .filter(({ familleDocumentId }) => cote_familleDocumentId === familleDocumentId)
             .flatMap(({ code: famille_code }) =>
               ({ famille: famille_code, cote: cote_code, type: type_code }))))
-    .sort(({ famille_1, cote_1, type_1 }, { famille_2, cote_2, type_2 }) => {
-      const famille_comparison = areStringsEqualsCaseInsensitive(famille_1, famille_2)
-      if (famille_comparison !== 0) {
-        famille_comparison;
-      }
-      const cote_comparison = areStringsEqualsCaseInsensitive(cote_1, cote_2)
-      if (cote_comparison !== 0) {
-        cote_comparison;
-      }
-      const type_comparison = areStringsEqualsCaseInsensitive(type_1, type_2)
-      if (type_comparison !== 0) {
-        type_comparison;
-      }
+    .sort((
+      { famille: famille_1, cote: cote_1, type: type_1 },
+      { famille: famille_2, cote: cote_2, type: type_2 }) => {
+      return localCompareCaseInsensitive(famille_1, famille_2) ??
+        localCompareCaseInsensitive(cote_1, cote_2) ??
+        localCompareCaseInsensitive(type_1, type_2)
     })
 // console.log(tryptiques)
-// process.exit(0)
+await fs.writeFile('C:/Users/deschaseauxr/Documents/DONUM/get_document_properties/tryptiques/tryptiques.json', JSON.stringify(tryptiques, undefined, 2))
+process.exit(0)
 const tryptiques_samples = ['DOCUMENTS COCOON', 'DOCUMENTS CONTRAT', 'DOCUMENTS EMOA'].map(famille_sample =>
   tryptiques.find(({ famille }) => areStringsEqualsCaseInsensitive(famille, famille_sample)))
 console.log(tryptiques_samples)
