a382a6aabf52310d189c26bfa5b68ef03252b821
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-10-13 10:28:22 +0200

    fixup

--- get_json_object_properties_sorted.mjs
+++ get_json_object_properties_sorted.mjs
@@ -3,8 +3,8 @@ import * as fs from 'fs/promises'
 import json_object from 'file://C:/Users/deschaseauxr/Documents/DONUM/document.json' assert { type: 'json' };
 import { randomUUID as get_globally_unique_identifier } from 'crypto'
 import { join as join_path } from 'path'
-const file_name = `${get_globally_unique_identifier({ disableEntropyCache: true })}.txt`
-const temp_file_path = join_path(get_temporary_directory(), file_name)
+const temp_file_name = `${get_globally_unique_identifier({ disableEntropyCache: true })}.txt`
+const temp_file_path = join_path(get_temporary_directory(), temp_file_name)
 const case_insensitive_strings_compare =
   (a, b) => a.localeCompare(b, undefined, { sensitivity: 'accent' })
 const alphabetically_sorted_object_properties =
