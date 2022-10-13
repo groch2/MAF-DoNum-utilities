905c557776d95a7479d341de872dcab99ab27944
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-10-13 10:27:51 +0200

    récupérer la liste des propriétés d'un objet triées par Ordralfabétix

--- get_json_object_properties_sorted.mjs
+++ get_json_object_properties_sorted.mjs
@@ -1,3 +1,14 @@
+import { tmpdir as get_temporary_directory } from 'os'
+import * as fs from 'fs/promises'
 import json_object from 'file://C:/Users/deschaseauxr/Documents/DONUM/document.json' assert { type: 'json' };
-const properties = Object.keys(json_object).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'accent' }))
-console.log(properties.join('\r\n'))
+import { randomUUID as get_globally_unique_identifier } from 'crypto'
+import { join as join_path } from 'path'
+const file_name = `${get_globally_unique_identifier({ disableEntropyCache: true })}.txt`
+const temp_file_path = join_path(get_temporary_directory(), file_name)
+const case_insensitive_strings_compare =
+  (a, b) => a.localeCompare(b, undefined, { sensitivity: 'accent' })
+const alphabetically_sorted_object_properties =
+  Object.keys(json_object).sort(case_insensitive_strings_compare).join('\r\n')
+await fs.writeFile(temp_file_path, alphabetically_sorted_object_properties)
+console.log({ temp_file_path })
+process.exit(0)
