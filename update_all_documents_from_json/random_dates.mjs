23499d90efb7a801b902a86719e49fdef94e29b1
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-10-27 15:34:10 +0200

    goodbye Kyle

--- update_all_documents_from_json/random_dates.mjs
+++ update_all_documents_from_json/random_dates.mjs
@@ -1,18 +1,13 @@
 import * as fs from 'fs/promises'
-function get_random_date() {
-  const time_min = new Date(1990, 0, 1).getTime()
-  const time_max = new Date(2022, 10, 20).getTime()
-  const time_interval = time_max - time_min
-  const random_value = time_min + Math.trunc(Math.random() * time_interval)
-  return new Date(JSON.stringify(new Date(random_value)).substring(1, 11))
-}
-import json_object from 'file://C:/Users/deschaseauxr/Documents/DONUM/documents.json' assert { type: 'json' };
-json_object.value = json_object.value.map(doc => {
-  return {
+import json_object from 'file://C:/Users/deschaseauxr/Documents/Donum/documents.json' assert { type: 'json' };
+import { get_random_date } from 'file://C:/Users/deschaseauxr/Documents/Donum/get_random_date.mjs'
+json_object.value = json_object.value.map(document => {
+  const document = {
     dateDocument: get_random_date(),
     deposeLe: get_random_date(),
-    ...doc,
+    ...document,
   }
+  return Object.fromEntries(Object.keys(document).sort().map(key => [key, document[key]]))
 })
 const stringified = JSON.stringify(json_object, undefined, 2)
 const file_path = 'C:/Users/deschaseauxr/Documents/DONUM/documents.json'
